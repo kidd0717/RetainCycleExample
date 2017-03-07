e01d1cb1488da11087a2ec905304d480958d4849
MMY <barry.ding@ichef.com.tw>
2017-03-07 18:24:49 +0800

    second commit

---
+++ RetainCycleExample3/RetainCycleExample3/TableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewCell.swift
+//  RetainCycleExample3
+//
+//  Created by Marcus Paze on 3/7/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+public protocol TableViewCellDelegate : NSObjectProtocol {
+    func tableViewCellDidSelectCell()
+}
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var pic: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    open var delegate: TableViewCellDelegate?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected == true {
+            self.delegate?.tableViewCellDidSelectCell()
+        }
+        
+        // Configure the view for the selected state
+    }
+}
